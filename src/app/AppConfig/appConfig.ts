cd63e4d80bfcde7aea7d059ef18b4f267ce70a22
govimu8 <muneeshkumar.g@payoda.com>
2022-09-29 10:42:00 +0530

    dist added

--- src/app/AppConfig/appConfig.ts
+++ src/app/AppConfig/appConfig.ts
@@ -1,6 +1,6 @@
 export class appConfig {
   //public readonly apiUrl = 'http://65.1.155.105/api/';
-  public readonly apiUrl = 'http://localhost:3000/api';
+  public readonly apiUrl = 'http://15.207.33.115:3000/api';
 
   public readonly login = 'admin/login';
   public readonly propertyAdd = 'property/add';
