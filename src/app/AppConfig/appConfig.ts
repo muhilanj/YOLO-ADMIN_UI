5841bd5e977f9d27c1d21d8c062083db82bc59eb
govimu8 <muneeshkumar.g@payoda.com>
2022-12-27 13:23:20 +0530

    test path

--- src/app/AppConfig/appConfig.ts
+++ src/app/AppConfig/appConfig.ts
@@ -2,7 +2,6 @@ export class appConfig {
   //public readonly apiUrl = 'http://65.1.155.105/api/';
   //public readonly apiUrl = 'http://15.207.33.115:3000/api';
   public readonly apiUrl = 'http://ec2-54-88-249-227.compute-1.amazonaws.com/yolo/api';
-
   public readonly login = 'admin/login';
   public readonly propertyAdd = 'property/add';
 }
